ee7973f343391506fdd01b754bea482f013a1c1d
Frank Anthony <frankez478@gmail.com>
2025-09-02 11:37:27 +0100

    feat: add HF API service

--- JS-Backend/server.js
+++ JS-Backend/server.js
@@ -1,9 +1,14 @@
 import express from "express";
+import cors from "cors"
+import dotenv from "dotenv"
 
+
+dotenv.config()
 const app = express();
 const PORT = 4500;
 
 app.use(express.json());
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("NFT backend is running");
---
+++ JS-Backend/services/huggingFaceServices.js
@@ -0,0 +1,4 @@
+import fetch from 'node-fetch'
+
+
+const HF_API_KEY = process.env.HUGGING_FACE_API_KEY
